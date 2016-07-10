2209758d93c9eb43bb6b67176d40ef80467c6c0f
A A Ba Loi Malaysia 1 (ip6s+) <trinhbaloi@petronas.com.my>
2016-07-10 15:44:54 +0800

    Initial Commit

---
+++ demoGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoGit
+//
+//  Created by Ba Loi Mac on 7/10/16.
+//  Copyright © 2016 AloCentral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoGit
+//
+//  Created by Ba Loi Mac on 7/10/16.
+//  Copyright © 2016 AloCentral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
